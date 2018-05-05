6eed19dcac98f88eb85424eab13716aec25ad5ea
Erika <37984498+ErikaVy@users.noreply.github.com>
2018-05-05 11:58:23 +0300

    Update main.js
    Prasom masteri! chekink :P

---
+++ Paprasta_uzduotis/main.js
@@ -0,0 +1,11 @@
+function myFunction() {
+    var first_name = prompt("What is your first name:", "first name");
+    var last_name = prompt("What is your last name:", "last name");
+    var age = prompt("How old are you:", "age");
+    if 	(first_name == "first name");
+    	(last_name == "last name");
+    	(age == "age"); {
+    	alert("My name is " + first_name + " " + last_name +  " and I am "  + age  + " years old!");
+   	};
+	};
+    console.log (myFunction);
